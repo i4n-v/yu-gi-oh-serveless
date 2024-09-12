ca31e854f0d2933234800553120e659d0e519ac6
Silvio Filho <silvio.filho@tascomtecnologia.com.br>
2024-09-11 21:44:52 -0300

    Merge branch 'main' of github.com:i4n-v/yu-gi-oh-serveless

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
 import { graphQLClient } from "@/lib/graphql-client";
-import { ICardArgs } from "../pages/api/_types";
 import { Transition } from "@headlessui/react";
+import { ICardArgs } from "@/pages/api/_types";
 
 const query = `
   query cards($page: Int!, $limit: Int!,$fname: String!) {
@@ -18,6 +18,7 @@ const query = `
         attribute
         card_images {
           image_url
+          image_url_cropped
         }
         card_prices{
           tcgplayer_price
@@ -38,7 +39,7 @@ export default function GraphQLRequest() {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
-  const getCards = async () => {
+  const getCards = async (page:number) => {
     const variables = {
       page: page,
       limit: 10,
@@ -78,7 +79,9 @@ export default function GraphQLRequest() {
   }, [cards]);
 
   useEffect(() => {
-    getCards();
+    if(page > 1){
+      getCards(page);
+    }
   }, [page]);
 
   const handleCardClick = (card: ICardArgs) => {
@@ -92,8 +95,8 @@ export default function GraphQLRequest() {
   };
 
   return (
-    <main className="flex w-full max-h-screen flex-col items-center gap-12 h-3/4">
-      <div className="w-full h-1/5 flex align-center gap-4 max-w-lg p-6 top-0 left-0 right-0 mx-auto shadow-lg z-50">
+    <div className="flex w-full max-h-screen flex-col items-center gap-12 h-3/4">
+      <div className="fixed w-full h-2/12 flex align-center gap-4 max-w-lg p-6 top-0 left-0 right-0 mx-auto shadow-lg z-50">
         <input
           type="text"
           placeholder="Search..."
@@ -101,14 +104,17 @@ export default function GraphQLRequest() {
           className="w-full py-2 px-4 text-gray-700 rounded-xl border border-gray-300 bg-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
         />
         <button
-          onClick={getCards}
+          onClick={() => {
+            setPage(1)
+            getCards(1)
+          }}
           className="px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
         >
           Search
         </button>
       </div>
       {isLoading && (
-        <div className="w-full h-screen flex justify-center items-center absolute">
+        <div className="w-full h-screen  flex justify-center items-center absolute">
           <Transition
             show={isLoading}
             enter="transition-opacity ease-in duration-500"
@@ -122,7 +128,7 @@ export default function GraphQLRequest() {
           </Transition>
         </div>
       )}
-      <div className="flex flex-wrap w-4/6 gap-4 h-2/5">
+      <div className="flex flex-wrap mt-[5%] justify-center w-4/6 gap-4 max-h-[80%]">
         {cards?.map((card, index) => (
           <Transition
             key={index}
@@ -131,20 +137,20 @@ export default function GraphQLRequest() {
             enter="transition-opacity transform duration-500 ease-in-out"
             enterFrom="opacity-0 scale-90"
             enterTo="opacity-100 scale-110"
-            className="inline-block w-2/12 h-fit"
+            className="inline-block lg:w-2/12 md:w-3/12 sm:w-4/12 max-h-[40%]"
           >
             <div
               key={index}
-              className="border border-gray-300 max-h-full rounded-lg overflow-hidden transition-transform duration-300 hover:scale-110 cursor-pointer hover:shadow-lg"
+              className="border border-gray-300 min-h-full rounded-lg overflow-hidden transition-transform duration-300 hover:scale-110 cursor-pointer hover:shadow-lg"
               onClick={() => handleCardClick(card)}
             >
               <img
-                src={card.card_images?.[0]?.image_url}
+                src={card.card_images?.[0]?.image_url_cropped}
                 alt={card.name}
                 className="w-full h-auto"
               />
               <div className="p-4">
-                <h3 className="font-semibold text-lg">{card.name}</h3>
+                <h3 className="font-semibold text-sm">{card.name}</h3>
                 <p className="text-gray-500">{card.type}</p>
               </div>
             </div>
@@ -152,7 +158,7 @@ export default function GraphQLRequest() {
         ))}
       </div>
       {!!cards.length && (
-        <div className="flex justify-between items-center w-full max-w-5xl mt-6">
+        <div className="fixed bottom-10 flex justify-between items-center w-full max-w-5xl mt-6">
           <button
             disabled={page === 1}
             className={`px-4 py-2 bg-blue-500 text-white rounded-lg transition ${
@@ -170,7 +176,9 @@ export default function GraphQLRequest() {
           <button
             disabled={page === totalPages}
             className={`px-4 py-2 bg-blue-500 text-white rounded-lg transition ${
-              page === totalPages ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-600"
+              page === totalPages
+                ? "opacity-50 cursor-not-allowed"
+                : "hover:bg-blue-600"
             }`}
             onClick={() => {
               setPage(page + 1);
@@ -183,8 +191,8 @@ export default function GraphQLRequest() {
       {/* Dialog Component */}
       {isDialogOpen && selectedCard && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-8 rounded-lg shadow-lg max-w-6xl w-full">
-            <div className="flex justify-end">
+          <div className="bg-white p-8 rounded-lg shadow-lg max-w-[80%] max-h-[80%] min-h-[80%] overflow-auto w-full">
+            <div className="flex justify-end fixed">
               <button
                 onClick={closeDialog}
                 className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition mb-4"
@@ -192,19 +200,30 @@ export default function GraphQLRequest() {
                 Fechar
               </button>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div className="flex justify-center items-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-20">
+              <div className="flex justify-center items-center ">
                 <img
                   src={selectedCard?.card_images?.[0]?.image_url}
                   alt={selectedCard.name}
-                  className="w-full h-auto object-cover hover:scale-150 max-w-md"
+                  onMouseOver={({ currentTarget }) => {
+                    currentTarget.src =
+                      selectedCard?.card_images?.[0]?.image_url_cropped!;
+                  }}
+                  onMouseLeave={({ currentTarget }) => {
+                    currentTarget.src =
+                      selectedCard?.card_images?.[0]?.image_url!;
+                  }}
+                  className="w-full h-auto object-cover hover:scale-105 sm:hover:scale-115 md:hover:scale-[1.55]  md:hover:mt-28 max-h-[100%] md:max-w-md sm:max-w-xs"
                 />
               </div>
-              <div className="flex flex-col gap-y-10 text-black">
-                <h2 className="text-2xl font-bold text-center mb-2">{selectedCard.name}</h2>
+              <div className="flex flex-col md:gap-y-10 sm:gap-y-1 text-black">
+                <h2 className="text-2xl font-bold text-center mb-2">
+                  {selectedCard.name}
+                </h2>
                 <div className="flex justify-between">
                   <div>
-                    <span className="font-semibold">Type:</span> {selectedCard.type}
+                    <span className="font-semibold">Type:</span>{" "}
+                    {selectedCard.type}
                   </div>
                   <div>
                     <span className="font-semibold">Market Price:</span>
@@ -216,19 +235,24 @@ export default function GraphQLRequest() {
                 {selectedCard?.type?.toLowerCase().includes("monster") && (
                   <div className="grid grid-cols-2 gap-4 mb-2">
                     <div>
-                      <span className="font-semibold">ATK:</span> {selectedCard.atk}
+                      <span className="font-semibold">ATK:</span>{" "}
+                      {selectedCard.atk}
                     </div>
                     <div>
-                      <span className="font-semibold">DEF:</span> {selectedCard.def}
+                      <span className="font-semibold">DEF:</span>{" "}
+                      {selectedCard.def}
                     </div>
                     <div>
-                      <span className="font-semibold">Level:</span> {selectedCard.level}
+                      <span className="font-semibold">Level:</span>{" "}
+                      {selectedCard.level}
                     </div>
                     <div>
-                      <span className="font-semibold">Race:</span> {selectedCard.race}
+                      <span className="font-semibold">Race:</span>{" "}
+                      {selectedCard.race}
                     </div>
                     <div>
-                      <span className="font-semibold">Attribute:</span> {selectedCard.attribute}
+                      <span className="font-semibold">Attribute:</span>{" "}
+                      {selectedCard.attribute}
                     </div>
                   </div>
                 )}
@@ -237,6 +261,6 @@ export default function GraphQLRequest() {
           </div>
         </div>
       )}
-    </main>
+    </div>
   );
 }
